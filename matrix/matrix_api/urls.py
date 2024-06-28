065f8d6f5d6a5018e6eb68f2ea6ff0bd20c68baa
Eslam Sayed <esla889900@gmail.com>
2024-06-28 07:17:26 +0300

    Update urls.py

--- matrix/matrix_api/urls.py
+++ matrix/matrix_api/urls.py
@@ -4,4 +4,15 @@ from .views import *
 
 urlpatterns = [
     path('userCheck/', user_handler.as_view(), name='user'),
+    path('image/', image_handler.as_view(), name='image'),
+    path('user/', USER.as_view(), name='user'),
+    path('sponsors/', SPONSORS.as_view(), name='sponsors'),
+    path('communities_partners/', COMMUNITIES_PARTNERS.as_view(),
+         name='communities_partners'),
+    path('partners/', PARTNERS.as_view(), name='partners'),
+    path('vips/', VIPS.as_view(), name='vips'),
+    path('mentors', MENTORS.as_view(), name='mentors'),
+    path('speakers', SPEAKERS.as_view(), name='speakers'),
+    path('host_and_main_communities', HOST_AND_MAIN_COMMUNITIES.as_view(),
+         name='host_and_main_communities'),
 ]
